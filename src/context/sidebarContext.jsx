74a26a6269e2f97a0b2e9a212173174a67601a15
sama-babaee-web <samababaee8866@gmail.com>
2024-02-04 18:54:05 +0330

    create sidebar

--- src/context/sidebarContext.jsx
+++ src/context/sidebarContext.jsx
@@ -1,17 +1,17 @@
-import { createContext , useReducer } from "react";
-import reducer from '../reducer/sidebarReducer';
-import PropTypes from "prop-types"
+// import { createContext , useReducer } from "react";
+// import reducer from '../reducer/sidebarReducer';
+// import PropTypes from "prop-types"
 
-const initialState ={
-    isSideOpen :false
-}
+// const initialState ={
+//     isSideOpen :false
+// }
 
-export const SidebarContext = createContext({});
-export const SidebarProvider = ({ children  }) => {
-    const [ state , dispatch ] = useReducer(reducer , initialState);
-    const togglesSidebar = () =>{
-        dispatch({
-            type:  "TOGGLE"
-        })
-    }
-}
+// export const SidebarContext = createContext({});
+// export const SidebarProvider = ({ children  }) => {
+//     const [ state , dispatch ] = useReducer(reducer , initialState);
+//     const togglesSidebar = () =>{
+//         dispatch({
+//             type:  "TOGGLE"
+//         })
+//     }
+// }
